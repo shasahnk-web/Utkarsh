e9430c5a6c9b8f2e2fbb52ba1677a6d9e3ba59b6
shasahnk-web <web.shashank.dev@GMAIL.COM>
2025-12-29 09:44:49 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 
+
 class Base(DeclarativeBase):
     pass
 
+
 db = SQLAlchemy(model_class=Base)
+
 
 class RequestedBatch(db.Model):
     id = db.Column(db.Integer, primary_key=True)
